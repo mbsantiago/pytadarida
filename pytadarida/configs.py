574b749156e9698044bccd4f1e22b47ab5ae2467
Santiago Martinez <santiago.mbal@gmail.com>
2023-01-28 16:30:08 +0000

    Added documentation to configs module

--- pytadarida/configs.py
+++ pytadarida/configs.py
@@ -1,5 +1,14 @@
+"""
+Configuration file for pytadarida.
+
+This file contains the path to the TadaridaD binary, as well as the
+default values for the command line arguments.
+
+"""
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TADARIDA_BINARY = os.path.join(BASE_DIR, "bin", "TadaridaD")
+"""The base directory of pytadarida."""
+
+TADARIDA_BINARY = os.path.join(BASE_DIR, "TadaridaD", "TadaridaD")
+"""The path to the TadaridaD binary."""
