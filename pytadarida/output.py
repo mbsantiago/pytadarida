3e6458187562cec3417d17956016974612ef3326
Santiago Martinez <santiago.mbal@gmail.com>
2023-01-28 14:57:11 +0000

    Better output loading functions with tests

--- pytadarida/output.py
+++ pytadarida/output.py
@@ -7,13 +7,48 @@ tadarida program into workable python objects.
 import csv
 import os
 import shutil
-from typing import List
-
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Iterable
+
+PathLike = str | os.PathLike
 
 
 @dataclass
 class DetectedSoundEvent:
+    """DetectedSoundEvent object.
+
+    Attributes
+    ----------
+    filename : str
+
+    call_number : int
+
+    version : int
+
+    file_duration : float
+
+    samplerate : int
+
+    start_time : float
+
+    duration : float
+
+    previous_start : float
+
+    min_freq : float
+
+    max_freq : float
+
+    band_width : float
+
+    master_point_freq : float
+
+    master_point_position : float
+
+    maximum_energy_frequency : float
+
+    """
     filename: str
     call_number: int
     version: int
@@ -30,60 +65,160 @@ class DetectedSoundEvent:
     maximum_energy_frequency: float
 
 
-def parse_ta_file(path: str, compression: bool = False):
-    with open(path, "r") as ta_file:
-        reader = csv.DictReader(ta_file, delimiter="\t")
-        return list(reader)
+def parse_ta_file(path: PathLike) -> list[DetectedSoundEvent]:
+    """Parse a .ta file into a list of DetectedSoundEvent objects.
+
+    Parameters
+    ----------
+    path : str or os.PathLike
+
+    Returns
+    -------
+    list of DetectedSoundEvent
+
+    Raises
+    ------
+    FileNotFoundError
+
+    """
+    path = Path(path)
+
+    if not path.exists():
+        raise FileNotFoundError(f"File {path} does not exist.")
+
+    with open(path, "r", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter="\t")
+        next(reader)  # skip header
+        return [
+            DetectedSoundEvent(
+                filename=row[0],
+                call_number=int(row[1]),
+                version=int(row[2]),
+                file_duration=float(row[3]),
+                samplerate=int(row[4]),
+                start_time=float(row[5]),
+                duration=float(row[6]),
+                previous_start=float(row[7]),
+                min_freq=float(row[8]),
+                max_freq=float(row[9]),
+                band_width=float(row[10]),
+                master_point_freq=float(row[11]),
+                master_point_position=float(row[12]),
+                maximum_energy_frequency=float(row[13]),
+            )
+            for row in reader
+        ]
 
 
-def get_output_file(
-    wav_file: str,
-    output_dir: str = "txt",
-    compression: bool = False,
-) -> str:
-    dirname = os.path.dirname(wav_file)
-    basename = os.path.basename(wav_file)
-    name, _ = os.path.splitext(basename)
-    path = os.path.join(dirname, output_dir, f"{name}.ta")
-    return path
+def get_wav_files(path: PathLike) -> list[Path]:
+    """Get a list of all .wav files in the given directory.
+
+    Parameters
+    ----------
+    path : str or os.PathLike
+
+    Returns
+    -------
+    list of str or os.PathLike
+
+    Raises
+    ------
+    FileNotFoundError
+
+    """
+    path = Path(path)
+    return list(path.glob("**/*.[wW][aA][vV]"))
 
 
-def get_output(files: List[str], compression=False):
-    results = []
+def get_output_files_from_path(
+    path: PathLike,
+) -> dict[PathLike, PathLike]:
+    """Get a dictionary of all .wav files in the given directory and their
+    corresponding .ta files.
+
+    If path is a directory, it returns a dictionary of all .wav files in the
+    directory and their corresponding .ta files.
+
+    Parameters
+    ----------
+    path : str or os.PathLike
+
+    Returns
+    -------
+    dict of str or os.PathLike
+
+    Raises
+    ------
+    FileNotFoundError
+
+    """
+    path = Path(path)
+
+    if path.is_file():
+        target = path.parent / "txt" / f"{path.stem}.ta"
+
+        if not target.exists():
+            raise FileNotFoundError(f"File {target} does not exist.")
+
+        return {path: target}
+
+    wav_files = get_wav_files(path)
+
+    ret = {}
+    for wav_file in wav_files:
+        target = wav_file.parent / "txt" / f"{wav_file.stem}.ta"
+
+        if not target.exists():
+            raise FileNotFoundError(f"File {target} does not exist.")
+
+        ret[wav_file] = target
+
+    return ret
+
+
+def get_output_files(
+    files: tuple[PathLike] | list[PathLike] | Iterable[PathLike],
+) -> dict[PathLike, PathLike]:
+    """Get a dictionary of all .wav files in the given list and their
+    corresponding .ta files.
+
+    Parameters
+    ----------
+    files : list of str or os.PathLike
+
+    Returns
+    -------
+    dict of str or os.PathLike
+
+    Raises
+    ------
+    FileNotFoundError
+
+    """
+    output_files = {}
     for filename in files:
-        path = get_output_file(filename, compression=compression)
-        results.extend(parse_ta_file(path, compression=compression))
-    return results
+        output_files.update(get_output_files_from_path(filename))
+    return output_files
 
 
-def clean_output(files: List[str], compression: bool = False):
-    for filename in files:
-        output_file = get_output_file(filename, compression=compression)
-        if os.path.exists(output_file):
-            os.remove(output_file)
+def clean_output(files: Iterable[PathLike]):
+    """Clean the output files.
 
-        directory = os.path.dirname(output_file)
-        if not os.listdir(directory):
-            os.rmdir(directory)
+    Tadarida-D creates a subdirectory named "txt" in the directory of each
+    processed audio file. The subdirectory contains a .ta file with the same
+    name as the audio files. This function deletes the associated .ta files
+    but nothing else. This function does not delete the audio file. If the
+    "txt" directory is empty after deleting the .ta files, it is also deleted.
 
-
-def move_output(files: List[str], output_dir: str = "txt", compression: bool = False):
-    for filename in files:
-        src = get_output_file(filename, compression=compression)
-        dst = get_output_file(filename, output_dir=output_dir, compression=compression)
-
-        if not os.path.exists(src):
-            continue
-
-        if src == dst:
-            continue
-
-        directory = os.path.dirname(dst)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        shutil.move(src, dst)
-
-        src_dir = os.path.directory(src)
-        if not os.listdir(src_dir):
-            os.rmdir(src_dir)
+    Parameters
+    ----------
+    files : list of str or os.PathLike
+        A file can be a wav file or a directory. If it is a directory, all
+        corresponding .ta files to the .wav files in the directory are deleted.
+    """
+    output_files = get_output_files(files)
+    for ta_file in output_files.values():
+        ta_file = Path(ta_file)
+        os.remove(ta_file)
+        if not list(ta_file.parent.glob("*")):
+            shutil.rmtree(ta_file.parent)
---
+++ tests/test_output.py
@@ -0,0 +1,286 @@
+"""Test tadarida output module.
+
+The Tadarida-D binary outputs a file for each processed audio file. The
+output file is located in a subdirectory of the input directory. The
+subdirectory is named "txt" and the output file has the same name as the
+input file but with the extension ".ta".
+
+This module tests the functions that:
+
+1. get the output files from the input files.
+2. parses the output files into python objects.
+3. writes the python objects to a .ta file.
+4. writes the python objects to a .csv file.
+5. clean up the output files.
+"""
+
+import os
+from pathlib import Path
+from typing import Iterable, List
+
+import pytest
+
+from pytadarida import output
+
+PathLike = str | os.PathLike
+
+
+def test_get_output_files(tmp_path: Path) -> None:
+    """Test get_output_files function."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    assert output.get_output_files(input_files) == output_files
+
+
+def test_get_output_files_raises_with_missing_output(tmp_path: Path) -> None:
+    """Test get_output_files raises with missing output."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Test
+    with pytest.raises(FileNotFoundError):
+        output.get_output_files(input_files)
+
+
+def test_get_output_files_on_directory(tmp_path: Path) -> None:
+    """Test get_output_files on directory."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    assert output.get_output_files([tmp_path]) == output_files
+
+
+def test_get_output_files_on_mixed_inputs(tmp_path: Path) -> None:
+    """Test get_output_files on mixed inputs."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "subdir" / "file2.wav",
+        tmp_path / "subdir" / "file3.wav",
+    ]
+
+    for file in input_files:
+        if file.parent != tmp_path:
+            file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    assert (
+        output.get_output_files([input_files[0], input_files[1].parent])
+        == output_files
+    )
+
+
+def test_clean_output_files(tmp_path: Path) -> None:
+    """Test clean_output_files function."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    output.clean_output(input_files)
+    for file in output_files.values():
+        assert not file.exists()
+
+
+def test_clean_output_files_does_not_delete_wav_files(tmp_path: Path) -> None:
+    """Test clean_output_files does not delete wav files."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    output.clean_output(input_files)
+    for file in input_files:
+        assert file.exists()
+
+
+def test_clean_output_files_does_not_delete_non_ta_files(
+    tmp_path: Path,
+) -> None:
+    """Test clean_output_files does not delete non-ta files."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Create non-ta files
+    non_ta_files = {
+        path: path.parent / "txt" / f"{path.stem}.txt" for path in input_files
+    }
+
+    for file in non_ta_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    output.clean_output(input_files)
+    for file in non_ta_files.values():
+        assert file.exists()
+
+
+def test_clean_output_deletes_txt_dir_when_empty(tmp_path: Path) -> None:
+    """Test clean_output deletes txt dir when empty."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    output.clean_output(input_files)
+    for file in output_files.values():
+        assert not file.parent.exists()
+
+
+def test_clean_output_does_not_delete_txt_dir_if_not_empty(
+    tmp_path: Path,
+) -> None:
+    """Test clean_output does not delete txt dir if not empty."""
+    # Create input files
+    input_files = [
+        tmp_path / "file1.wav",
+        tmp_path / "file2.wav",
+        tmp_path / "file3.wav",
+    ]
+
+    for file in input_files:
+        file.touch()
+
+    # Create output files
+    output_files = {
+        path: path.parent / "txt" / f"{path.stem}.ta" for path in input_files
+    }
+
+    for file in output_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Create non-ta files
+    non_ta_files = {
+        path: path.parent / "txt" / f"{path.stem}.txt" for path in input_files
+    }
+
+    for file in non_ta_files.values():
+        file.parent.mkdir(exist_ok=True)
+        file.touch()
+
+    # Test
+    output.clean_output(input_files)
+    for file in non_ta_files.values():
+        assert file.parent.exists()
+
+
+def test_parse_ta_file_has_correct_output(tmp_path: Path) -> None:
+    """Test parse_ta_file has correct output."""
+    # Create input file
+    input_file = tmp_path / "file1.ta"
+    input_file.touch()
