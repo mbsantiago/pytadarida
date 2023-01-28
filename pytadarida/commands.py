ac4c5241aa85ebc28827c75b941a17988fe6aa71
Santiago Martinez <santiago.mbal@gmail.com>
2023-01-28 16:29:38 +0000

    first version of run_tadarida command with tests

--- pytadarida/commands.py
+++ pytadarida/commands.py
@@ -1,18 +1,36 @@
+"""Run the tadarida binary on the given files.
+
+This module contains the run_tadarida function, which runs the tadarida binary
+on the given files.
+
+"""
+import os
 import subprocess
+from typing import Iterable, Literal
+
+import pandas as pd
 
 from pytadarida.configs import TADARIDA_BINARY
+from pytadarida.logs import RunStatus, get_run_status
+from pytadarida.output import get_output_files, clean_output_files
+from pytadarida.parsing import parse_detections
+from pytadarida.validate_inputs import validate_files
+
+__all__ = [
+    "run_tadarida",
+]
 
 
-def _run_command(*args, capture_output=False):
+PathLike = str | os.PathLike
+
+
+def _run_command(*args: str, capture_output: bool = False):
     result = subprocess.run(
         [TADARIDA_BINARY, *args],
         capture_output=capture_output,
+        check=True,
     )
     return result.stdout
-
-
-def command_help():
-    return _run_command("-h").decode("utf-8")
 
 
 def _build_args(
@@ -20,7 +38,6 @@ def _build_args(
     time_expansion=1,
     features=2,
     frequency_band=1,
-    compression=False,
 ):
     args = [
         "-t",
@@ -33,30 +50,94 @@ def _build_args(
         str(frequency_band),
     ]
 
-    if compression:
-        args.append("-c")
-
     return args
 
 
-def run_tadarida_binary(
-    files,
-    threads=1,
-    time_expansion=1,
+def run_tadarida(
+    files: PathLike | list[PathLike] | tuple[PathLike] | Iterable[PathLike],
+    threads: int = 1,
+    time_expansion: Literal[10, 1] = 1,
     features=2,
-    frequency_band=1,
-    compression=False,
-):
-    if not isinstance(files, (list, tuple)):
+    frequency_band: Literal[1, 2] = 1,
+) -> tuple[pd.DataFrame, RunStatus]:
+    """Run the tadarida binary on the given files.
+
+    Will run the tadarida binary on the given files, and return a dataframe
+    with the detected sound events.
+
+    Parameters
+    ----------
+    files : str or list of str
+        Either a directory path containing .wav files or a list of .wav files,
+        to be processed. Relative or absolute paths can be used. Audio files
+        must be short. Accepted limit depends on sound file characteristics
+        (between 6.4 and 12.8 seconds in high frequency (HF) mode, and between
+         32 and 64 seconds in low frequency (LF) mode).
+    threads : int, optional
+        Allows to execute n parallel threads (1 by default).
+        Note that 1 thread consumes approximately 150 MB of memory.
+    time_expansion : float, optional
+        Time expansion factor, either 10 (default) for 10-times expanded .wav
+        files (most commonly used in bat monitoring) or 1 for direct
+        recordings.
+    features : int, optional
+        sets the list of features to be extracted on each detected sound
+        event.
+    frequency_band : int, optional
+        Frequency bands to be used; n = 2 allows to treat low frequencies
+        (0.8 to 25 kHz) whereas n=1 (default) treats high frequencies
+        (8 to 250 kHz).
+
+    Returns
+    -------
+    detections: pd.DataFrame
+        Dataframe with detected sound events.
+    status: RunStatus
+        A RunStatus object containing the stdout and stderr of the tadarida
+        binary.
+
+    Raises
+    ------
+    FileNotFoundError
+
+    """
+    if isinstance(files, (str, os.PathLike)):
         files = [files]
+
+    validate_files(files)
 
     args = _build_args(
         threads=threads,
         time_expansion=time_expansion,
         features=features,
         frequency_band=frequency_band,
-        compression=compression,
     )
 
     args = [*args, *files]
-    return _run_command(*args, capture_output=True)
+
+    _run_command(*args, capture_output=False)
+
+    status = get_run_status()
+
+    try:
+        outputs = get_output_files(files)
+    except FileNotFoundError as error:
+        raise FileNotFoundError(
+            "The tadarida binary did not produce any output files.\n"
+            f"Tadarida-D Output: {status.stdout}\n"
+            f"Error: {status.error}\n"
+        ) from error
+
+    try:
+        detections = parse_detections(outputs)
+
+        # Remove the output files after parsing
+        clean_output_files(files)
+    except FileNotFoundError as error:
+        raise FileNotFoundError(
+            "Error parsing the output files.\n"
+            f"Tadarida-D Output: {status.stdout}\n"
+            f"Error: {status.error}\n"
+        ) from error
+
+    return detections, status
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,18 +1,61 @@
-from pytadarida.commands import command_help
+"""Tests for pytadarida.commands"""
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
 from pytadarida.commands import run_tadarida
+from pytadarida.logs import RunStatus
+
+DATA_DIR = Path(__file__).parent / "data"
+
+TEST_WAV = DATA_DIR / "Barbastella_barbastellus_1_s.wav"
+TEST_TA = DATA_DIR / "ta_file_version_1.ta"
+TEST_DIR_WAVS = DATA_DIR / "dir_of_wavs"
 
 
-def test_command_help_tadarida():
-    help = command_help()
-    print(help)
-    assert help
-    assert False
+def test_run_tadarida_works_on_wav_file():
+    """Test run_tadarida works on a wav file."""
+    assert TEST_WAV.exists()
+    run_tadarida(TEST_WAV)
 
 
-def test_run_tadarida():
-    file = "/home/santiago/Datasets/lib_chiroptera/5aa403e95d5b148aa61389a3.WAV"
-    results = run_tadarida(file)
+def test_run_tadarida_fails_on_non_existing_file():
+    """Test run_tadarida fails on a non existing file."""
+    test_file = DATA_DIR / "non_existing_file.wav"
+    assert not test_file.exists()
+    with pytest.raises(FileNotFoundError):
+        run_tadarida(test_file)
 
-    print(results)
 
-    assert False
+def test_run_tadarida_fails_on_non_wav_file():
+    """Test run_tadarida fails on a non-wav file."""
+    assert TEST_TA.exists()
+    with pytest.raises(ValueError):
+        run_tadarida(TEST_TA)
+
+
+def test_run_tadarida_return_type():
+    """Test run_tadarida returns the detection dataframe and runstatus"""
+    assert TEST_WAV.exists()
+    detections, status = run_tadarida(TEST_WAV)
+    assert isinstance(detections, pd.DataFrame)
+    assert isinstance(status, RunStatus)
+
+def test_run_tadarida_works_on_dir_of_wavs():
+    """Test run_tadarida works on a dir of wav files."""
+    assert TEST_DIR_WAVS.exists()
+    run_tadarida(TEST_DIR_WAVS)
+
+
+def test_no_log_files_after_run_tadarida():
+    """Test that no log files are created after running tadarida."""
+    assert TEST_WAV.exists()
+    run_tadarida(TEST_WAV)
+    assert not any(Path("log").glob("*.log"))
+
+def test_output_files_are_cleared_after_run_tadarida():
+    """Test that output files are cleared after running tadarida."""
+    assert TEST_WAV.exists()
+    run_tadarida(TEST_WAV)
+    assert not (TEST_WAV.parent / "txt").exists()
